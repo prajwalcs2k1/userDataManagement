5032a90e5430f4f6461e07fa96cd079f96c7336c
prajwal6553731 <prajwal@tunerlabs.com>
2024-01-18 13:04:56 +0530

    login api implemented

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -43,7 +43,7 @@ exports.userRegister = async (req,res) => {
             password : hashedPassword
         })
         await newUser.save()
-        const userToken = jwt.sign({id : newUser.id, name : newUser.name, password : newUser.password}, process.env.SECRET_KEY)
+        const userToken = jwt.sign({id : newUser.id, name : newUser.name}, process.env.SECRET_KEY)
 
         return res.status(201).send(jsonResponse.success(userToken,"successfully signed up"))
     }
@@ -51,4 +51,38 @@ exports.userRegister = async (req,res) => {
         console.log(err)
         return res.status(503).send(jsonResponse.failure(err,"database connection error... try again later"))
     }   
+}
+
+// User Login
+exports.userLogin = async (req,res) => {
+    const {userName,password} = req.body
+
+    const data = {
+        name : userName,
+        password : password
+    }
+    const rules =  {
+        name : "required",
+        password : "required"
+    }
+    const validation = new Validator(data, rules)
+    if(validation.fails()) return res.status(400).send(jsonResponse.failure({errors:validation.errors.all()},"details not provided properly"))
+
+    try{
+        const requiredUser = await fetchUserDataByName(userName)
+        
+        if(requiredUser == null) return res.status(404).send(jsonResponse.failure(null,"user does not exist"))
+        
+        // compare the password provided by the user with that stored in the table
+        if(await bcrypt.compare(password,requiredUser.password)){
+            const userToken = jwt.sign({id : requiredUser.id, name : requiredUser.name},process.env.SECRET_KEY)
+            return res.status(200).send(jsonResponse.success(userToken,"logged in successfully"))
+        }else{
+            return res.status(400).send(jsonResponse.failure(null,"wrong password")) 
+        }
+    }
+    catch(err){
+        console.log(err)
+        return res.status(503).send(jsonResponse.failure(err,"database connection error... try again later"))
+    }    
 }
--- src/routers/user.js
+++ src/routers/user.js
@@ -6,4 +6,8 @@ const routerUser =  express.Router()
 // User SignUp 
 routerUser.post("/register",controllerUser.userRegister)
 
+// User Login 
+routerUser.post("/login",controllerUser.userLogin)
+
+
 module.exports = routerUser
